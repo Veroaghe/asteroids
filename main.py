b10ae36a4bcf439efd368795159665aad72ad801
Glenn Verhaeghe <veroaghe@gmail.com>
2025-03-02 13:43:29 +0100

    added groups and a shadow_elite

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import pygame
 from constants import *
 from player import Player
+import random
 
 def main():
     pygame.init()
     print("Starting Asteroids!")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    # add_shadow_elite()
 
     while True:
         for event in pygame.event.get():
@@ -16,10 +25,23 @@ def main():
                 return
 
         screen.fill(BLACK)
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+
+
+def add_shadow_elite():
+    for _ in range(200):
+        color = (
+            random.randrange(0, 256),
+            random.randrange(0, 256),
+            random.randrange(0, 256),
+        )
+        Player(random.randrange(0, SCREEN_WIDTH), random.randrange(0, SCREEN_HEIGHT), color)
 
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -3,9 +3,10 @@ from circleshape import CircleShape
 from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 
 class Player(CircleShape):
-    def __init__(self, x, y):
+    def __init__(self, x, y, color="white"):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.color = color
     
     # in the player class
     def triangle(self):
@@ -17,7 +18,7 @@ class Player(CircleShape):
         return [a, b, c]
 
     def draw(self, screen):
-        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+        pygame.draw.polygon(screen, self.color, self.triangle(), 2)
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt

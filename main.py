5a1b814f95e4d27985005e14a519acac3e2d7c70
Glenn Verhaeghe <veroaghe@gmail.com>
2025-03-02 11:40:03 +0100

    set up pygame.init

--- main.py
+++ main.py
@@ -2,8 +2,29 @@ import pygame
 from constants import *
 
 def main():
+    screen = init()
+    draw(screen)
+
+def init():
+    pygame.init()
     print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    # print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    return screen
+
+def draw(screen):
+    while True:
+        if events():
+            break
+        pygame.Surface.fill(screen, BLACK)
+        pygame.display.flip()
+
+def events():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return True
+    return False
+
 
 if __name__ == "__main__":
     main()

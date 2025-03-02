af5460e017fed207a775ef4e019108b8a4243e98
Glenn Verhaeghe <veroaghe@gmail.com>
2025-03-02 13:00:18 +0100

    added player rotation

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def main():
                 return
 
         screen.fill(BLACK)
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000

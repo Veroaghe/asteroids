4cae9fba8c8b1999e44e1e4982dbcda7b413a37e
Glenn Verhaeghe <veroaghe@gmail.com>
2025-03-02 12:52:32 +0100

    player added

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     print("Starting Asteroids!")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     clock = pygame.time.Clock()
     dt = 0
 
@@ -14,6 +16,7 @@ def main():
                 return
 
         screen.fill(BLACK)
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
